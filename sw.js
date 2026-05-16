a43af342ab9166b53ddc1efce7c956a7c49e48c9
LemusML7 <115673646+LemusML7@users.noreply.github.com>
2026-05-15 22:21:57 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,10 @@
-const CACHE_NAME = 'sanisidro-v1';
-const urlsToCache = ['./Cotizador.html'];
+const CACHE_NAME = 'cotizador-v1';
+const urlsToCache = ['./', './index.html', './manifest.json', './launchericon-192x192.png', './launchericon-512x512.png'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
